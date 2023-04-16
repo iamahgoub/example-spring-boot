5afeb1938a6372704243836943c99c55b6837b33
EC2 Default User <ec2-user@ip-172-31-26-127.eu-west-2.compute.internal>
2023-04-16 15:10:59 +0000

    adding run-service.sh

---
+++ scripts/run-service.sh
@@ -0,0 +1,6 @@
+# run java
+java -XX:CRaCRestoreFrom=/opt/crac-files
+
+# the code below is to keep the container running indefinitely
+echo Executing an infinite loop to keep the container running...
+while true; do sleep 1; done
